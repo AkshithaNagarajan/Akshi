c9752220572baef87d2880cb99fc4179d48b31c4
ShanmugavelGanesh <44182631+ShanmugavelGanesh@users.noreply.github.com>
2024-06-18 13:03:17 +0530

    my first

--- SampleProgram/src/main/java/com/SampleTask/SampleProgram/SampleProgramApplication.java
+++ SampleProgram/src/main/java/com/SampleTask/SampleProgram/SampleProgramApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleProgramApplication {
 
+
+//
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProgramApplication.class, args);
 	}
